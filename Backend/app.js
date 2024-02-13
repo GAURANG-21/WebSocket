7bad39022c0f428466f29a6adccdf59cc1909f36
GAURANG-21 <gaurang2621@gmail.com>
2024-02-14 03:02:33 +0530

    feat: Created circuit/io and connected to frontend

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,32 @@
 import express from "express";
 const app = express();
 import configServer from "./config/configServer.js";
+import { Server } from "socket.io";
 import { createServer } from "node:http";
 
 const { PORT } = configServer;
 
 // Creating the server instance "app" which internally handles the incoming request and generate the response
 const server = createServer(app);
+
+// To create a socket/circuit
+const io = new Server(server,{
+  cors:{
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+  console.log("UserId", socket.id);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
--- Client/WebSocket/src/App.jsx
+++ Client/WebSocket/src/App.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
 function App() {
+
+  //If no URL is given in io, then it will host the io OR assume the io(circuit) onto the same server it is running
+  const socket = io('http://localhost:3000/');
+
+  useEffect(()=>{
+    socket.on('connect', ()=>{
+      console.log("Connected", socket.id);
+    })
+  }, [])
   return (
     <>
       <h1>Hello World!</h1>
