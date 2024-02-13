69c54cbf4d4326fd917b70ed090b6e4a9e926dd2
GAURANG-21 <gaurang2621@gmail.com>
2024-02-14 03:29:18 +0530

    feat: disconnect, emit and broadcast

--- Client/WebSocket/src/App.jsx
+++ Client/WebSocket/src/App.jsx
@@ -2,15 +2,23 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 function App() {
-
   //If no URL is given in io, then it will host the io OR assume the io(circuit) onto the same server it is running
-  const socket = io('http://localhost:3000/');
+  const socket = io("http://localhost:3000/");
 
-  useEffect(()=>{
-    socket.on('connect', ()=>{
+  useEffect(() => {
+    socket.on("connect", () => {
       console.log("Connected", socket.id);
-    })
-  }, [])
+    });
+
+    socket.on("Welcome", (data) => {
+      console.log(data);
+    });
+
+    //Before the react refreshes, the cleanup function of useEffect runs first
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <>
       <h1>Hello World!</h1>
