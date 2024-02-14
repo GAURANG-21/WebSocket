6a1206d5ceaba13bcbafed39f91716c1873e9c2f
GAURANG-21 <gaurang2621@gmail.com>
2024-02-14 23:56:22 +0530

    add: Displaying messages on frontend

--- Client/WebSocket/src/App.jsx
+++ Client/WebSocket/src/App.jsx
@@ -1,17 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Stack,
+} from "@mui/material";
 
 function App() {
   //If no URL is given in io, then it will host the io OR assume the io(circuit) onto the same server it is running
-  const socket = io("http://localhost:3000/");
+  const socket = useMemo(() => io("http://localhost:3000/"), []);
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("Message", { message, room });
+    setMessage("");
+  };
 
   useEffect(() => {
     socket.on("connect", () => {
+      setSocketId(socket.id);
       console.log("Connected", socket.id);
     });
 
     socket.on("Welcome", (data) => {
       console.log(data);
+    });
+
+    socket.on("welcome", (data) => {
+      console.log(data);
+    });
+
+    socket.on("receive-message", (data) => {
+      setMessages((messages) => [...messages, data]);
+      console.log("Message received and the message is:", data);
     });
 
     //Before the react refreshes, the cleanup function of useEffect runs first
@@ -20,9 +48,46 @@ function App() {
     };
   }, []);
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{ height: 200 }} />
+      <Typography variant="h1" component={"div"} gutterBottom>
+        Welcome to the Socket.IO
+      </Typography>
+      <Typography variant="h2" component={"div"} gutterBottom>
+        {socketId}
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+        <Button type="submit" color="primary" variant="contained">
+          Send
+        </Button>
+      </form>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room"
+          variant="outlined"
+        />
+        <Button type="submit" color="primary" variant="contained">
+          Send
+        </Button>
+      </form>
+      <Stack>
+        {messages.map((m, i) => (
+          <Typography key={i} variant="h6" component="div" gutterBottom>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
+    </Container>
   );
 }
 
