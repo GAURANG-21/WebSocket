9895394270bb49dc8b473f72adc42c2bdc5a23c8
GAURANG-21 <gaurang2621@gmail.com>
2024-06-18 23:13:58 +0530

    add: Final commit

--- Client/WebSocket/src/App.jsx
+++ Client/WebSocket/src/App.jsx
@@ -16,11 +16,18 @@ function App() {
   const [room, setRoom] = useState("");
   const [socketId, setSocketId] = useState("");
   const [messages, setMessages] = useState([]);
+  const [roomName, setRoomName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     socket.emit("Message", { message, room });
     setMessage("");
+  };
+
+  const joinRoom = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
   };
 
   useEffect(() => {
@@ -56,6 +63,20 @@ function App() {
       <Typography variant="h2" component={"div"} gutterBottom>
         {socketId}
       </Typography>
+
+      <form onSubmit={joinRoom}>
+        <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+        <Button type="submit" color="primary" variant="contained">
+          Send
+        </Button>
+      </form>
+
       <form onSubmit={handleSubmit}>
         <TextField
           value={message}
